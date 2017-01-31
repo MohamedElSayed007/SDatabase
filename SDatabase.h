14cb4b2e5f420b6d6b2af7bb2ab1cd1cb8d14ec3
Mohamed El Sayed <m.elsayed4420@gmail.com>
2017-02-01 01:49:28 +0200

    Update SDatabase.h

--- SDatabase.h
+++ SDatabase.h
@@ -1,9 +1,8 @@
 /*
  * SDatabase.h
  *
- *  Created on: Jan 22, 2015
  *      Author: Mohamed El Sayed
- *      email : engmohamedelsayed007@gmail.com
+ *      email : m.elsayed4420@gmail.com
  *      lowlevelcode.blogspot.com
  */
 
@@ -20,23 +19,25 @@ using namespace std;
 
 class SDatabase{
 private:
-string reader(const string);
-void writer(string);
-string dbName;
+	bool first_time = true;
+	string reader(const string);
+	void writer(string);
+	string dbName="sdb_d.txt";
+	bool check_db_init();
 
 public:
-bool enable_duplication = false;
-bool exists(const string);
-string id, name, address;
-void createDB(string);
-bool addNew();
-bool deleteById(const string);
-bool searchById(const string);
-int viewAll();
-void clear();
+	int entry_count=20;
+	bool enable_duplication = true;
+	bool exists(const string);
+	string id;
+	string *entries = new string[entry_count];
+	void createDB(string);
+	bool addNew();
+	bool deleteById(const string);
+	bool searchById(const string);
+	int viewAll();
+	void clear();
 };
-
-
 
 
 #endif /* SDATABASE_H_ */
